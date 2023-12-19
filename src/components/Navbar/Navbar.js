d742ad255e64785e67b0c1165e96853d6d841774
Daniel Okoro <danielochinasa@gmail.com>
2023-12-19 19:58:54 +0100

    Complete Dashboard

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,77 +1,230 @@
-import React, {useState, useEffect} from "react";
-import {Nav, NavbarContainer, HamburgerIcon, NavMenu, NavItemBtn, NavBtnLink} from "./Navbar.elements";
-import {FaTimes, FaBars} from "react-icons/fa";
-import {IconContext} from "react-icons/lib";
-import {MainButton, OutlineButton} from "../../globalStyles";
+import React, { useState, useEffect } from "react";
+import {
+  Nav,
+  NavbarContainer,
+  HamburgerIcon,
+  NavMenu,
+  NavItemBtn,
+  NavBtnLink,
+} from "./Navbar.elements";
+import { FaTimes, FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { MainButton, OutlineButton } from "../../globalStyles";
 
-import {ReactComponent as Logo} from "../../images/logo.svg";
-import {Link} from "react-router-dom";
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import { Link } from "react-router-dom";
+import { Button, Flex } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/actions";
+import { useHistory } from "react-router-dom";
+import { Typography } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import { Menu, Dropdown, Space } from "antd";
+const { Title } = Typography;
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const history = useHistory();
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.isAuthenticated);
+  const user = useSelector((state) => state.user);
+  const userFirstName = user?.user?.firstName || "";
+  const userLastName = user?.user?.lastName || "";
+  const userBal = user?.user?.walletBalance || "";
 
-    const handleClick = () => setClick(!click);
+  //   console.log(
+  //     "Redux State:",
+  //     useSelector((state) => state)
+  //   );
 
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
 
-    const showButton = () => {
-        // so if the screensize is <= 960px then set button state to false
-        if (window.innerwidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
+  const closeMobileMenu = () => setClick(false);
 
-    useEffect(() => {
-        showButton();
-    }, []);
+  const showButton = () => {
+    // so if the screensize is <= 960px then set button state to false
+    if (window.innerwidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-    window.addEventListener("resize", showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
 
+  window.addEventListener("resize", showButton);
+  const handleLogout = () => {
+    // Dispatch the logout action when the user clicks the logout button
+    dispatch(logout());
+    history.push("/");
+  };
+
+  const items = [
+    {
+      key: "1",
+      label: (
+        <a href="/main-dashboard" style={{ textDecoration: "none" }}>
+          Dashboard
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      label: "Profile",
+    },
+    {
+      key: "3",
+      label: <span onClick={handleLogout}>LogOut</span>,
+    },
+  ];
+
+  const UserDropdown = () => {
     return (
-        <>
-            <IconContext.Provider value={{color: "#000"}}>
-                <Nav>
-                    <NavbarContainer>
-                        <Link to="/">
-                            <Logo style={{marginTop: "10px"}} />
-                        </Link>
-                        <HamburgerIcon onClick={handleClick}>{click ? <FaTimes /> : <FaBars />}</HamburgerIcon>
-                        <NavMenu onClick={handleClick} click={click}>
-                            <NavItemBtn>
-                                {button ? (
-                                    <NavBtnLink to="/login">
-                                        <OutlineButton type="primary">LOGIN</OutlineButton>
-                                    </NavBtnLink>
-                                ) : (
-                                    <NavBtnLink to="/login">
-                                        <OutlineButton onClick={closeMobileMenu} fontBig primary>
-                                            LOGIN
-                                        </OutlineButton>
-                                    </NavBtnLink>
-                                )}
-                            </NavItemBtn>
-                            <NavItemBtn>
-                                {button ? (
-                                    <NavBtnLink to="/sign-up">
-                                        <MainButton type="primary">SIGN UP</MainButton>
-                                    </NavBtnLink>
-                                ) : (
-                                    <NavBtnLink to="/sign-up">
-                                        <MainButton onClick={closeMobileMenu} fontBig primary>
-                                            SIGN UP
-                                        </MainButton>
-                                    </NavBtnLink>
-                                )}
-                            </NavItemBtn>
-                        </NavMenu>
-                    </NavbarContainer>
-                </Nav>
-            </IconContext.Provider>
-        </>
+      <Dropdown
+        menu={{
+          items,
+        }}
+        placement="bottomLeft"
+        arrow
+      >
+        <Space
+          key="user-dropdown"
+          style={{
+            cursor: "pointer",
+            border: "1px solid #d9d9d9",
+            padding: "8px",
+            borderRadius: "8px", // Set borderRadius to make it a square
+          }}
+          size={20}
+        >
+          <img
+            src="https://placekitten.com/40/40" // Replace with your circular image URL
+            alt="User Avatar"
+            style={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              marginRight: "8px",
+            }}
+          />
+          <DownOutlined />
+        </Space>
+      </Dropdown>
     );
+  };
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#000" }}>
+        <Nav>
+          <NavbarContainer>
+            <Link to="/">
+              <Logo style={{ marginTop: "10px" }} />
+            </Link>
+            <HamburgerIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </HamburgerIcon>
+
+            <NavMenu onClick={handleClick} click={click}>
+              {isAuthenticated && (
+                <NavItemBtn>
+                  <NavBtnLink to="/dashboard">
+                    <MainButton type="primary">
+                      Identity Verification
+                    </MainButton>
+                  </NavBtnLink>
+                </NavItemBtn>
+              )}
+              {!isAuthenticated && (
+                <>
+                  <NavItemBtn>
+                    {button ? (
+                      <NavBtnLink to="/login">
+                        <OutlineButton type="primary">LOGIN</OutlineButton>
+                      </NavBtnLink>
+                    ) : (
+                      <NavBtnLink to="/login">
+                        <OutlineButton
+                          onClick={closeMobileMenu}
+                          fontBig
+                          primary
+                        >
+                          LOGIN
+                        </OutlineButton>
+                      </NavBtnLink>
+                    )}
+                  </NavItemBtn>
+                  <NavItemBtn>
+                    {button ? (
+                      <NavBtnLink to="/sign-up">
+                        <MainButton type="primary">SIGN UP</MainButton>
+                      </NavBtnLink>
+                    ) : (
+                      <NavBtnLink to="/sign-up">
+                        <MainButton onClick={closeMobileMenu} fontBig primary>
+                          SIGN UP
+                        </MainButton>
+                      </NavBtnLink>
+                    )}
+                  </NavItemBtn>
+                </>
+              )}
+
+              {/* Conditionally render logout button when the user is authenticated */}
+              {window.innerWidth <= 960
+                ? isAuthenticated && (
+                    <>
+                      <NavItemBtn>
+                        <NavBtnLink to="/main-dashboard">
+                          <MainButton type="primary">Dashboard</MainButton>
+                        </NavBtnLink>
+                      </NavItemBtn>
+                      <NavItemBtn>
+                        <NavBtnLink to="/main-dashboard">
+                          <MainButton type="primary">Profile</MainButton>
+                        </NavBtnLink>
+                      </NavItemBtn>
+                      <NavItemBtn>
+                        <NavBtnLink>
+                          <OutlineButton type="primary" onClick={handleLogout}>
+                            LogOut
+                          </OutlineButton>
+                        </NavBtnLink>
+                      </NavItemBtn>
+                    </>
+                  )
+                : isAuthenticated && (
+                    <>
+                      <div
+                        style={{
+                          marginTop: "20px",
+                          textAlign: "end",
+                          paddingLeft: "15px",
+                          paddingRight: "15px",
+                        }}
+                      >
+                        <Title level={4}>
+                          {userFirstName} {userLastName}
+                        </Title>
+                        <p>
+                          Bal:
+                          <span style={{ color: "#0DC939" }}>
+                            ₦{userBal.toLocaleString()}
+                          </span>
+                        </p>
+                      </div>
+                      <UserDropdown />
+                    </>
+                  )}
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
 export default Navbar;
--- src/pages/dashboard/mainDashboard.jsx
+++ src/pages/dashboard/mainDashboard.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Card, Row, Col, Statistic, Space, Table, Tag } from "antd";
-import { Container, Heading4, InfoSec, Subtitle } from "../../globalStyles";
+import React, { useState } from "react";
+import { Card, Row, Col, Statistic, Space, Table, Tag, Tabs } from "antd";
+import { Container, Heading4, InfoSec, MainButton } from "../../globalStyles";
+import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
 
 const columns = [
   {
@@ -72,19 +73,53 @@ const data = [
     expires: ["2024/03/16"],
   },
 ];
+const onChange = (key) => {
+  console.log(key);
+};
+const items = [
+  {
+    key: "1",
+    label: "Verification History",
+    children: (
+      <Row>
+        <Col span={24}>
+          <Table columns={columns} dataSource={data} />
+        </Col>
+      </Row>
+    ),
+  },
+  {
+    key: "2",
+    label: "Transaction Logs",
+    children: "Content of Tab Pane 2",
+  },
+];
+
+const config = {
+  public_key: "FLWPUBK-**************************-X",
+  tx_ref: Date.now(),
+  amount: 100,
+  currency: "NGN",
+  payment_options: "card,mobilemoney,ussd",
+  customer: {
+    email: "user@gmail.com",
+    phone_number: "070********",
+    name: "john doe",
+  },
+  customizations: {
+    title: "my Payment Title",
+    description: "Payment for items in cart",
+    logo: "https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg",
+  },
+};
+
+// const handleFlutterPayment = useFlutterwave(config);
 
 const MainDashboard = () => {
+  const handleFlutterPayment = useFlutterwave(config);
   return (
     <Container>
       <InfoSec>
-        {/* <Card
-          style={{
-            width: "100%",
-          }}
-        >
-          <Heading4>Dashboard</Heading4>
-          <Subtitle>Contains all previous activities carried out</Subtitle>
-        </Card> */}
         <Row gutter={20}>
           <Col
             span={6}
@@ -93,12 +128,22 @@ const MainDashboard = () => {
             md={{ span: 8 }}
             lg={{ span: 8 }}
           >
-            <Card style={{ backgroundColor: "#DFF7E4", marginTop: "10px" }}>
+            <Card
+              style={{
+                marginTop: "10px",
+                border: "3px #0DC939 solid",
+                borderRadius: "12px",
+                border: "5px solid #09C93A",
+                background: "#EBFFF0",
+              }}
+            >
               <Statistic
                 title="Total verification "
-                value={124}
+                value={500}
                 valueStyle={{
                   color: "#3f8600",
+                  fontSize: "50px",
+                  fontWeight: "700",
                 }}
               />
             </Card>
@@ -110,12 +155,22 @@ const MainDashboard = () => {
             md={{ span: 8 }}
             lg={{ span: 8 }}
           >
-            <Card style={{ backgroundColor: "#DFF7E4", marginTop: "10px" }}>
+            <Card
+              style={{
+                marginTop: "10px",
+                border: "3px #0DC939 solid",
+                borderRadius: "12px",
+                border: "5px solid #09C93A",
+                background: "#EBFFF0",
+              }}
+            >
               <Statistic
                 title="Successful verification "
-                value={124}
+                value={450}
                 valueStyle={{
                   color: "#3f8600",
+                  fontSize: "50px",
+                  fontWeight: "700",
                 }}
               />
             </Card>
@@ -127,24 +182,47 @@ const MainDashboard = () => {
             md={{ span: 8 }}
             lg={{ span: 8 }}
           >
-            <Card style={{ backgroundColor: "#DFF7E4", marginTop: "10px" }}>
+            <Card
+              style={{
+                marginTop: "10px",
+                border: "3px #0DC939 solid",
+                borderRadius: "12px",
+                border: "5px solid #09C93A",
+                background: "#EBFFF0",
+              }}
+            >
               <Statistic
                 title="Failed verification "
-                value={124}
+                value={50}
                 valueStyle={{
                   color: "#3f8600",
+                  fontSize: "50px",
+                  fontWeight: "700",
                 }}
               />
             </Card>
           </Col>
         </Row>
         <div style={{ marginTop: "30px" }}>
-          <Heading4>Recent Activities</Heading4>
-          <Row>
-            <Col span={24}>
-              <Table columns={columns} dataSource={data} />
-            </Col>
-          </Row>
+          <Space direction="horizontal" align="center">
+            <Heading4>Recent Activities</Heading4>
+            <MainButton
+              type="primary"
+              onClick={() => {
+                handleFlutterPayment({
+                  callback: (response) => {
+                    console.log(response);
+                    closePaymentModal(); // this will close the modal programmatically
+                  },
+                  onClose: () => {},
+                });
+              }}
+            >
+              Fund Wallet
+            </MainButton>
+          </Space>
+
+          <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
         </div>
       </InfoSec>
     </Container>
