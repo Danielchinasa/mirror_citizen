24190fda1729d25d0d151a8d3755840744e408b9
Daniel Okoro <danielochinasa@gmail.com>
2024-01-08 10:28:30 +0100

    ignore build

---
+++ build/precache-manifest.9a91dd5f6e27f9847377065ee9d56678.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b3c3a061d30f51400358478ff8ca2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d4bf1af8884a3dbc808",
+    "url": "./static/js/2.5527cccd.chunk.js"
+  },
+  {
+    "revision": "a293ab2bbf2b87119e0ce7e31dd7e7b3",
+    "url": "./static/js/2.5527cccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea0044d88012e1c4697",
+    "url": "./static/js/main.2e8832c7.chunk.js"
+  },
+  {
+    "revision": "a8ee77dc14a7afc62100",
+    "url": "./static/js/runtime-main.71d77cd9.js"
+  },
+  {
+    "revision": "83116b9d044e5b9f245f405272e37a65",
+    "url": "./static/media/Corporate.83116b9d.svg"
+  },
+  {
+    "revision": "9055b56ae871dae706d1e574e07beac6",
+    "url": "./static/media/banner.9055b56a.png"
+  },
+  {
+    "revision": "decf5bb411a541a360b8e32462257247",
+    "url": "./static/media/hero-2.decf5bb4.svg"
+  },
+  {
+    "revision": "6bec09d44fc68cc55da28be10f203731",
+    "url": "./static/media/individual.6bec09d4.svg"
+  },
+  {
+    "revision": "82ff32ba1b567cc59aa1b6fd03500ff7",
+    "url": "./static/media/lady.82ff32ba.svg"
+  },
+  {
+    "revision": "4c3da6f15b4d37160a3f8c1f70ed7676",
+    "url": "./static/media/logo.4c3da6f1.svg"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "./static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "0eb722607fdc7ae44fbb095a58cb7400",
+    "url": "./static/media/reg.0eb72260.jpg"
+  },
+  {
+    "revision": "25167a53701fed0398d680f46a00685c",
+    "url": "./static/media/slide.25167a53.svg"
+  },
+  {
+    "revision": "0953f78a629e03d9fec3e838a4f171fb",
+    "url": "./static/media/slide2.0953f78a.svg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "./static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "./static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "./static/media/svg-3.397c455a.svg"
+  }
+]);
