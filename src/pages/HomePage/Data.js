db6bd8a03806d98b67b161a43812cbd85e99e360
Daniel Okoro <danielochinasa@gmail.com>
2024-02-27 08:09:34 +0100

    Clean Site cache

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -8,7 +8,7 @@ export const homeObjOne = {
   description: "How it works",
   buttonLabel: "Verify Identity",
   imgStart: "",
-  img: require("../../images/hero-2.svg"),
+  img: require("../../images/hero-2.png"),
   alt: "Credit Card",
   start: "",
 };
