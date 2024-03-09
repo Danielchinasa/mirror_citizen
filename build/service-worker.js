bb430a64946313ea700c5be13115b92d060f2111
Daniel Okoro <danielochinasa@gmail.com>
2024-03-09 17:47:05 +0100

    changed to http

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.952a3af6062861c609839cbac3d3d8d1.js"
+  "./precache-manifest.0ad243537ca5cbfd0d5d73a09df2101f.js"
 );
 
 self.addEventListener('message', (event) => {
