91e661e1f29923627b6bc05c5ecd7613e8e10259
Daniel Okoro <danielochinasa@gmail.com>
2024-03-12 22:55:35 +0100

    update company name

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.052a3dcc5783a544d3d45f22f2ad4257.js"
+  "./precache-manifest.630dc0111a5924b474b97660fe1f31a2.js"
 );
 
 self.addEventListener('message', (event) => {
