ca753bccce042b4abdc1e1afd5f3b21a9163ab9d
PND <elroifrancis@gmail.com>
2024-03-13 19:50:34 +0100

    reset password page

---
+++ build/precache-manifest.d73e69745ab25851af6042d6e932da2a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1585f42590e52e2b3aa2271f3b18acd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3dc17616471858b8b852",
+    "url": "./static/css/2.ec61a722.chunk.css"
+  },
+  {
+    "revision": "0a2a133401028dfd1e12",
+    "url": "./static/css/main.d54bfbb9.chunk.css"
+  },
+  {
+    "revision": "3dc17616471858b8b852",
+    "url": "./static/js/2.cfcff9d6.chunk.js"
+  },
+  {
+    "revision": "a310d8629474179b6bf5944bf32788eb",
+    "url": "./static/js/2.cfcff9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2a133401028dfd1e12",
+    "url": "./static/js/main.1b133ba6.chunk.js"
+  },
+  {
+    "revision": "a8ee77dc14a7afc62100",
+    "url": "./static/js/runtime-main.71d77cd9.js"
+  },
+  {
+    "revision": "83116b9d044e5b9f245f405272e37a65",
+    "url": "./static/media/Corporate.83116b9d.svg"
+  },
+  {
+    "revision": "dfc673b153f3f4275652ce15bf398b5a",
+    "url": "./static/media/banner.dfc673b1.png"
+  },
+  {
+    "revision": "3132370c4e7abbdc1814a9ecbde0cbe6",
+    "url": "./static/media/car-insurance.3132370c.svg"
+  },
+  {
+    "revision": "020b3b46079eb5abf52a8670b18854ce",
+    "url": "./static/media/clearvin.020b3b46.png"
+  },
+  {
+    "revision": "2a1a7bdb379dce0f7d51ada99e0adeef",
+    "url": "./static/media/contact.2a1a7bdb.png"
+  },
+  {
+    "revision": "acbe8d8e0f587748bdaeac98f3cb2b3b",
+    "url": "./static/media/credit-card.acbe8d8e.svg"
+  },
+  {
+    "revision": "1a817a95a42d8c43031378d122a05ffe",
+    "url": "./static/media/defaultDp.1a817a95.png"
+  },
+  {
+    "revision": "ef04cacc99584ec2f6deb5d312131c24",
+    "url": "./static/media/e_citizen_Data_Protection_and_Privacy_Policy_FINAL.ef04cacc.pdf"
+  },
+  {
+    "revision": "95e5acb2fbc7647e64063132bc9894f0",
+    "url": "./static/media/flutterwave-logos-idVM8GW1LQ.95e5acb2.png"
+  },
+  {
+    "revision": "d757ba0873c63f8bbfe22867f096327b",
+    "url": "./static/media/hero-2.d757ba08.png"
+  },
+  {
+    "revision": "6bec09d44fc68cc55da28be10f203731",
+    "url": "./static/media/individual.6bec09d4.svg"
+  },
+  {
+    "revision": "82ff32ba1b567cc59aa1b6fd03500ff7",
+    "url": "./static/media/lady.82ff32ba.svg"
+  },
+  {
+    "revision": "e1e01b752a6058eddeda02e266e19a82",
+    "url": "./static/media/lady.e1e01b75.png"
+  },
+  {
+    "revision": "4c3da6f15b4d37160a3f8c1f70ed7676",
+    "url": "./static/media/logo.4c3da6f1.svg"
+  },
+  {
+    "revision": "411010b5ea4654ad00ca167bfaa588cd",
+    "url": "./static/media/ndpr.411010b5.png"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "./static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "0eb722607fdc7ae44fbb095a58cb7400",
+    "url": "./static/media/reg.0eb72260.jpg"
+  },
+  {
+    "revision": "25167a53701fed0398d680f46a00685c",
+    "url": "./static/media/slide.25167a53.svg"
+  },
+  {
+    "revision": "0953f78a629e03d9fec3e838a4f171fb",
+    "url": "./static/media/slide2.0953f78a.svg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "./static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "./static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "./static/media/svg-3.397c455a.svg"
+  }
+]);
