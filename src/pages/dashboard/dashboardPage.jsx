5e393d21057a559e9099627a6f19429337e61d51
Daniel Okoro <danielochinasa@gmail.com>
2024-03-14 01:10:54 +0100

    fixed all cases for user instance

--- src/pages/dashboard/dashboardPage.jsx
+++ src/pages/dashboard/dashboardPage.jsx
@@ -1034,6 +1034,8 @@ const DashboardPage = () => {
               ? totalServiceCost
               : currencyCheck === "USD" && userCurrency === "ngn"
               ? totalveriNiara
+              : currencyCheck === "USD" && userCurrency === "usd"
+              ? totalServiceCost
               : userCurrency == "NGN" && currencyCheck != "NGN"
               ? outsideNgWithNiaraPrice
               : // : userCurrency == "USD" && currencyCheck === "USD"
