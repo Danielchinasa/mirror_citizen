2d7e668c5acbc1c430a79eba7f3bd28db28e1b44
Daniel Okoro <danielochinasa@gmail.com>
2024-04-28 20:50:17 +0100

    created the sms webhook build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9824456b507e9aae953bce81df6fb0ff.js"
+  "/precache-manifest.c7046934859241e39dc56c4ce890137b.js"
 );
 
 self.addEventListener('message', (event) => {
