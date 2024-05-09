e4917bfe3980ba13774c4e544261a3a3d3fb6f31
Daniel Okoro <danielochinasa@gmail.com>
2024-05-09 11:45:36 +0100

    Added new checks for IOS

--- src/firebase.js
+++ src/firebase.js
@@ -1,35 +1,39 @@
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBExfy-yXA7OxLaC7HrbzmTzw8gd1j1E-w",
+  authDomain: "ecitizen-23c16.firebaseapp.com",
+  projectId: "ecitizen-23c16",
+  storageBucket: "ecitizen-23c16.appspot.com",
+  messagingSenderId: "504541434686",
+  appId: "1:504541434686:web:d094029ccfbf06f192926f",
+  measurementId: "G-E4J0093Z41",
+};
 const userAgent = navigator.userAgent;
 
-// Function to check if the user is using an iPhone or iPad
+// initializeApp(firebaseConfig);
+let messaging = null;
+const app = initializeApp(firebaseConfig);
 const isiOS = () => {
   return /iPad|iPhone|iPod/.test(userAgent);
 };
-
-let Sendrequest, onMessageListener;
-
 if (!isiOS()) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBExfy-yXA7OxLaC7HrbzmTzw8gd1j1E-w",
-    authDomain: "ecitizen-23c16.firebaseapp.com",
-    projectId: "ecitizen-23c16",
-    storageBucket: "ecitizen-23c16.appspot.com",
-    messagingSenderId: "504541434686",
-    appId: "1:504541434686:web:d094029ccfbf06f192926f",
-    measurementId: "G-E4J0093Z41",
-  };
+  messaging = getMessaging(app);
+}
 
-  // Initialize Firebase only if not using iOS
-  const app = initializeApp(firebaseConfig);
-  const messaging = getMessaging(app);
-
-  Sendrequest = () => {
-    console.log("Requesting User Permission......");
+export const Sendrequest = () => {
+  console.log("Requesting User Permission......");
+  try {
     Notification.requestPermission().then((permission) => {
       if (permission === "granted") {
         console.log("Notification User Permission Granted.");
+
         return getToken(messaging, {
           vapidKey: `BM_Ct83FGrfjBlQMsG_Ekfe5kwl2bs-hzsRhvI28KQxOuVCIlwiLP2PanQWCh7AV9OKKsYgK5R1ACrF4PvCxyRE`,
         })
@@ -51,21 +55,16 @@ if (!isiOS()) {
         console.log("User Permission Denied.");
       }
     });
-  };
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-  onMessageListener = () => {
-    return new Promise((resolve) => {
-      onMessage(messaging, (payload) => {
-        resolve(payload);
-      });
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
     });
-  };
+  });
 
-  Sendrequest();
-} else {
-  console.log(
-    "User is using an iPhone or iPad. Skipping Firebase initialization."
-  );
-}
-
-export { Sendrequest, onMessageListener };
+Sendrequest();
