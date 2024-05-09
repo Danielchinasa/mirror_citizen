3f1df0b41a0a69427e68ad6d203b20e32f1abc41
Daniel Okoro <danielochinasa@gmail.com>
2024-05-09 10:08:29 +0100

    Row back changes for the IOS Check

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
-const userAgent = navigator.userAgent;
-
-// Function to check if the user is using an iPhone or iPad
-const isiOS = () => {
-  return /iPad|iPhone|iPod/.test(userAgent);
-};
-
-// Load Firebase script only if user is not using an iPhone or iPad
-
 const firebaseConfig = {
   apiKey: "AIzaSyBExfy-yXA7OxLaC7HrbzmTzw8gd1j1E-w",
   authDomain: "ecitizen-23c16.firebaseapp.com",
@@ -20,56 +11,44 @@ const firebaseConfig = {
   measurementId: "G-E4J0093Z41",
 };
 
-// Initialize Firebase only if not using iOS
+// initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-// Define functions related to Firebase only if not using iOS
 export const Sendrequest = () => {
-  if (!isiOS()) {
-    console.log("Requesting User Permission......");
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        console.log("Notification User Permission Granted.");
+  console.log("Requesting User Permission......");
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      console.log("Notification User Permission Granted.");
 
-        return getToken(messaging, {
-          vapidKey: `BM_Ct83FGrfjBlQMsG_Ekfe5kwl2bs-hzsRhvI28KQxOuVCIlwiLP2PanQWCh7AV9OKKsYgK5R1ACrF4PvCxyRE`,
+      return getToken(messaging, {
+        vapidKey: `BM_Ct83FGrfjBlQMsG_Ekfe5kwl2bs-hzsRhvI28KQxOuVCIlwiLP2PanQWCh7AV9OKKsYgK5R1ACrF4PvCxyRE`,
+      })
+        .then((currentToken) => {
+          if (currentToken) {
+            console.log("Client Token: ", currentToken);
+            localStorage.setItem("clientToken", currentToken);
+          } else {
+            console.log("Failed to generate the registration token.");
+          }
         })
-          .then((currentToken) => {
-            if (currentToken) {
-              console.log("Client Token: ", currentToken);
-              localStorage.setItem("clientToken", currentToken);
-            } else {
-              console.log("Failed to generate the registration token.");
-            }
-          })
-          .catch((err) => {
-            console.log(
-              "An error occurred when requesting to receive the token.",
-              err
-            );
-          });
-      } else {
-        console.log("User Permission Denied.");
-      }
-    });
-  } else {
-    console.log("User is using an iPhone or iPad. Skipping Firebase script.");
-  }
+        .catch((err) => {
+          console.log(
+            "An error occurred when requesting to receive the token.",
+            err
+          );
+        });
+    } else {
+      console.log("User Permission Denied.");
+    }
+  });
 };
 
-export const onMessageListener = () => {
-  if (!isiOS()) {
-    return new Promise((resolve) => {
-      onMessage(messaging, (payload) => {
-        resolve(payload);
-      });
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
     });
-  } else {
-    console.log("User is using an iPhone or iPad. Skipping Firebase script.");
-    // Return a resolved promise to maintain consistent return type
-    return Promise.resolve();
-  }
-};
+  });
 
 Sendrequest();
