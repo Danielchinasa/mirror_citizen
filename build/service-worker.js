37594cba580e26b8fe6ca158658064bcf39b255d
Daniel Okoro <danielochinasa@gmail.com>
2024-05-16 12:09:35 +0100

    Added a new Ads from Reach

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d545ac391ea02f41524f8ec4e58655d9.js"
+  "/precache-manifest.85bb0b11fb6917552674349c26104f78.js"
 );
 
 self.addEventListener('message', (event) => {
