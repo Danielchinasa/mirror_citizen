ece6c976bac63fdd6bde3ee011f653633631359d
PND <elroifrancis@gmail.com>
2026-06-25 20:01:09 +0100

    standardize backend api

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -3,8 +3,11 @@
 // const imageBaseUrl = "https://e-citizen.ng:8443";
 
 //Prod API
-const baseUrl = "https://e-citizen.ng:8444/api/v2";
-const imageBaseUrl = "https://e-citizen.ng:8444";
+// const baseUrl = "https://e-citizen.ng:8444/api/v2";
+// const imageBaseUrl = "https://e-citizen.ng:8444";
+
+const baseUrl = "https://api.e-citizen.ng/api/v2";
+const imageBaseUrl = "https://api.e-citizen.ng";
 
 export default baseUrl;
 export { imageBaseUrl };
