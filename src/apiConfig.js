5b27da55b7f289a856022882c4261ac344ef53e4
PND <elroifrancis@gmail.com>
2026-06-30 13:43:06 +0100

    update staging host url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 //Staging API
-const baseUrl = "https://e-citizen.ng:8443/api/v2";
-const imageBaseUrl = "https://e-citizen.ng:8443";
+const baseUrl = "https://api-staging.e-citizen.ng/api/v2";
+const imageBaseUrl = "https://api-staging.e-citizen.ng";
 
 //Prod API
 // const baseUrl = "https://e-citizen.ng:8444/api/v2";
