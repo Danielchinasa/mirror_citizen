03217bb52d51895137680aefacb781a020bb482f
Daniel Okoro <danielochinasa@gmail.com>
2026-07-24 10:10:42 +0100

    Update country links in Navbar for Ghana, Kenya, Uganda, and Côte d'Ivoire

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -177,7 +177,7 @@ function Navbar() {
       >
         <Menu.Item key="country-ghana">
           <a
-            href="https://e-citizen.africa"
+            href="https://e-citizen.africa/gh"
             target="_blank"
             rel="noopener noreferrer"
             style={{ textDecoration: "none" }}
@@ -194,7 +194,7 @@ function Navbar() {
         </Menu.Item>
         <Menu.Item key="country-kenya">
           <a
-            href="https://e-raia.com"
+            href="https://e-raia.com/ke"
             target="_blank"
             rel="noopener noreferrer"
             style={{ textDecoration: "none" }}
@@ -211,7 +211,7 @@ function Navbar() {
         </Menu.Item>
         <Menu.Item key="country-uganda">
           <a
-            href="https://e-raia.africa"
+            href="https://e-raia.africa/ug"
             target="_blank"
             rel="noopener noreferrer"
             style={{ textDecoration: "none" }}
@@ -228,7 +228,7 @@ function Navbar() {
         </Menu.Item>
         <Menu.Item key="country-civ">
           <a
-            href="https://citoyen.africa"
+            href="https://citoyen.africa/ci"
             target="_blank"
             rel="noopener noreferrer"
             style={{ textDecoration: "none" }}
@@ -311,7 +311,7 @@ function Navbar() {
     <Menu>
       <Menu.Item key="country-ghana">
         <a
-          href="https://e-citizen.africa"
+          href="https://e-citizen.africa/gh"
           target="_blank"
           rel="noopener noreferrer"
           style={{ textDecoration: "none" }}
@@ -324,7 +324,7 @@ function Navbar() {
       </Menu.Item>
       <Menu.Item key="country-kenya">
         <a
-          href="https://e-raia.com"
+          href="https://e-raia.com/ke"
           target="_blank"
           rel="noopener noreferrer"
           style={{ textDecoration: "none" }}
@@ -337,7 +337,7 @@ function Navbar() {
       </Menu.Item>
       <Menu.Item key="country-uganda">
         <a
-          href="https://e-raia.africa"
+          href="https://e-raia.africa/ug"
           target="_blank"
           rel="noopener noreferrer"
           style={{ textDecoration: "none" }}
@@ -350,7 +350,7 @@ function Navbar() {
       </Menu.Item>
       <Menu.Item key="country-civ">
         <a
-          href="https://citoyen.africa"
+          href="https://citoyen.africa/ci"
           target="_blank"
           rel="noopener noreferrer"
           style={{ textDecoration: "none" }}
